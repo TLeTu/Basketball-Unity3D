c4fa19066c61efe1d98e629e2a05ac54110bcee2
TLeTu <tranletu2003@gmail.com>
2025-07-29 16:20:06 +0700

    Remove unecessary assets

--- Assets/_Scripts/BallController.cs
+++ Assets/_Scripts/BallController.cs
@@ -2,59 +2,27 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private bool isHoldingBall = false;
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private Rigidbody rb;
-
-    public float throwForceMultiplier = 1.5f;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.isKinematic = true;
-    }
+    private bool isDragging = false;
+    private float distance;
 
     void OnMouseDown()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            isHoldingBall = true;
-            startPosition = Input.GetTouch(0).position;
-            Debug.Log("Player is holding the ball.");
-        }
-        else if (Input.GetMouseButtonDown(0))
-        {
-            isHoldingBall = true;
-            startPosition = Input.mousePosition;
-            Debug.Log("Player is holding the ball with mouse.");
-        }
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        isDragging = true;
     }
 
     void OnMouseUp()
     {
-        if (isHoldingBall)
-        {
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                endPosition = Input.GetTouch(0).position;
-            }
-            else
-            {
-                endPosition = Input.mousePosition;
-            }
-
-            isHoldingBall = false;
-            ThrowBall();
-            Debug.Log("Player released the ball.");
-        }
+        isDragging = false;
     }
 
-    void ThrowBall()
+    void Update()
     {
-        rb.isKinematic = false;
-        Vector3 throwDirection = (endPosition - startPosition);
-        Vector3 force = new Vector3(throwDirection.x, throwDirection.y, throwDirection.y) * throwForceMultiplier;
-        rb.AddForce(force);
+        if (isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
     }
 }
