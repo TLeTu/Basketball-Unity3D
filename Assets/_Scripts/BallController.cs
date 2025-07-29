0272fe517bcec9dd1635eea5a4bca862c4a391f5
TLeTu <tranletu2003@gmail.com>
2025-07-30 00:30:48 +0700

    Drag the rb instead of directly modifying the pos

--- Assets/_Scripts/BallController.cs
+++ Assets/_Scripts/BallController.cs
@@ -1,21 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BallController : MonoBehaviour
 {
-    Vector3 _mousePosition;
+    private Vector3 _mousePosition;
+    private Rigidbody _rb;
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _mainCamera = Camera.main;
+    }
 
     private Vector3 GetMousePos()
     {
-        return Camera.main.WorldToScreenPoint(transform.position);
+        if (_mainCamera == null)
+        {
+            return Vector3.zero;
+        }
+        return _mainCamera.WorldToScreenPoint(transform.position);
     }
     
     private void OnMouseDown() {
         Debug.Log("Clicking");
         _mousePosition = Input.mousePosition - GetMousePos();
+        _rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        _rb.isKinematic = false;
     }
 
     private void OnMouseDrag() {
+        if (_mainCamera == null)
+        {
+            return;
+        }
         Debug.Log("Dragging");
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - _mousePosition);
+        _rb.MovePosition(_mainCamera.ScreenToWorldPoint(Input.mousePosition - _mousePosition));
     }
 }
